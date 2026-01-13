f7ec2282d77d1ee5449cf530b45ec9ee9bafadb5
Jesaiah Prayor <jesaiahprayor@jesaiahs-mbp.mynetworksettings.com>
2026-01-13 16:40:49 -0500

    Update types

--- src/components/HighlightsList.tsx
+++ src/components/HighlightsList.tsx
@@ -3,14 +3,14 @@ import type { RefObject } from "react";
 
 interface Props {
   highlights: Highlight[];
-  videoRef: RefObject<HTMLVideoElement> | null;
+  videoRef: RefObject<HTMLVideoElement>;
 }
 
 
 
 export default function HighlightsList({ highlights, videoRef }: Props) {
     function playClip(start: number, end: number) {
-        const video = videoRef && videoRef.current;
+        const video = videoRef.current;
         if (!video) return;
       
         video.currentTime = start;
